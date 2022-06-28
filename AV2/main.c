b3382ace04352fb5d3094894760ef56a4515b4d7
0zono <73228452+0zono@users.noreply.github.com>
2022-06-28 17:34:17 -0400

    arrumando problema com caracteres
    adicionando windows.h para que caracteres especiais possam ser incluídos

--- AV2/main.c
+++ AV2/main.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include "menus.c"
+#include  <windows.h>
 
 int main() {
+    SetConsoleOutputCP(65001); //permitir a inclusão de caracteres especiais no console.
     menu();
     return 0;
 }
